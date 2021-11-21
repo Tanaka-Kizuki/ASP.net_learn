0c94e6a958a2cc83f1d2d8e31e9121b6eeefabc3
Tanaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-11-21 17:50:39 +0900

    RazorApp クエリ値取得

---
+++ SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SampleRazorApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public string Message { get; set; } = "This is sample message";
+        private string[][] data = new string[][] {
+            new string[]{"taro","taro@yamada"},
+            new string[]{"hanako","hanako@flower"},
+            new string[]{"sachiko","sachiko@happy"}
+        };
+
+        [BindProperty(SupportsGet = true)]
+        public int id { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "This is the added Message";
+        }
+
+        public string getData(int id)
+        {
+            string[] target = data[id];
+            return "[名前：" + target[0] + "メール" + target[1] + "]";
+        }
+    }
+}
