ac270009663735c808c9a34dff34755a1e7ca5af
Tanaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-11-21 17:03:59 +0900

    セッションの利用

--- SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
+++ SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,25 @@ namespace SampleMVCApp.Controllers
 {
     public class HelloController : Controller
     {
+        //Session
+        [HttpGet("Hello/{id?}/{name?}")]
+        public IActionResult Index(int id,string name)
+        {
+            ViewData["message"] = "※セッションにIDとNameを保存しました";
+            HttpContext.Session.SetInt32("id", id);
+            HttpContext.Session.SetString("name", name);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Other()
+        {
+            ViewData["id"] = HttpContext.Session.GetInt32("id");
+            ViewData["name"] = HttpContext.Session.GetString("name");
+            ViewData["message"] = "保存されたセッションの値を表示します。";
+            return View("Index");
+        }
+
         // GET: /<controller>/
         //public List<string> list;
 
@@ -22,14 +42,14 @@ namespace SampleMVCApp.Controllers
 
         //}
 
-        [Route("Hello/{id?}/{name?}")]
-        public IActionResult Index(int id,string name)
-        {
-            ViewData["message"] = "id=" + id + "name=" + name;
-            //ViewData["list"] = "";
-            //ViewData["listdata"] = list;
-            return View();
-        }
+        //[Route("Hello/{id?}/{name?}")]
+        //public IActionResult ndex(int id,string name)
+        //{
+        //    ViewData["message"] = "id=" + id + "name=" + name;
+        //    //ViewData["list"] = "";
+        //    //ViewData["listdata"] = list;
+        //    return View();
+        //}
 
         //[HttpPost]
         //public IActionResult Form()
