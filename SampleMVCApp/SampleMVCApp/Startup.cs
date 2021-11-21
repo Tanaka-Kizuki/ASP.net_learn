e51347d3cf1eb82d8a354ad1f0c809f219a85e3f
Tanaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-11-21 15:17:53 +0900

    viewへ値渡し

--- SampleMVCApp/SampleMVCApp/Startup.cs
+++ SampleMVCApp/SampleMVCApp/Startup.cs
@@ -29,6 +29,7 @@ namespace SampleMVCApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //開発モードかリリースモードか判断
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
